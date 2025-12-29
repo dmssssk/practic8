beb2a3f41231bc9bc6a0f1dec8947cca694bc4f1
dimassssek <dimaivashin2908@gmail.com>
2025-12-30 00:57:02 +0500

    v1

---
+++ Information.cs
@@ -0,0 +1,29 @@
+﻿namespace practic_8;
+
+public record Information(
+    string accessLevel,
+    string id,
+    string name,
+    string parentId,
+    string ownerId,
+    string[] children
+)
+{
+    public void ShowInformation()
+    {
+        string childs = string.Join(", ", children);
+        string information =
+            $"""
+             name: {name}
+             id: {id}
+             owner id: {ownerId}
+             parent id: {parentId}
+             acess level: {accessLevel}
+             acess level: {accessLevel}
+             childrens: {(childs == "" ? "null" :childs)}
+             """;
+        
+     Console.Write(information);   
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Unicode;
+using System.Text.Encodings.Web;
+
+using practic_8;
+
+
+HttpClient http = new HttpClient();
+string api = "https://seadox.ru/api/seadocs";
+
+
+var options = new JsonSerializerOptions
+{
+    Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+    WriteIndented = true
+};
+
+Console.Write("id: ");
+string id = Console.ReadLine()!;
+
+var request = await http.GetAsync($"{api}/{id}");
+
+var json = JsonNode.Parse(await request.Content.ReadAsStringAsync());
+
+var result = JsonSerializer.Deserialize<Information>(json);
+
+result.ShowInformation();
+
